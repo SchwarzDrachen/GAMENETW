3609f6134bb5653fbc76830c125224fea4dee227
siararisa-iac <126797704+siararisa-iac@users.noreply.github.com>
2024-09-07 11:41:49 +0800

    Added Enemy Spawning and UI Setup

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField] private float moveSpeed = 5.0f;
+
+    private Transform target = null;
+
+    private void OnEnable(){
+        LookAtTarget();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void SetTarget(Transform target){
+        this.target = target;
+    }
+
+    private void Update(){
+        Move();
+    }
+    
+    private void LookAtTarget(){
+        Quaternion newRotation;
+        Vector3 targetDirection = target == null ? transform.position : transform.position - target.transform.position;
+        newRotation = Quaternion.LookRotation(targetDirection, Vector3.forward);
+        newRotation.x = 0;
+        newRotation.y = 0;
+        transform.rotation = newRotation;
+    }
+
+    private void Move(){
+        transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
     }
 }
--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -4,5 +4,60 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    
+    [SerializeField] private float minSpawnInterval = 0.50f;
+    [SerializeField] private float maxSpawnInterval = 2.50f;
+
+    private float spawnInterval;
+    private Boundary boundary;
+    private Coroutine spawner;
+
+    private void Start(){
+        boundary = new();
+        boundary.CalculateScreenRestrictions();
+    }
+
+    private void Update(){
+        if(spawner != null){
+            return;
+        }
+        spawner = StartCoroutine(SpawnCoroutine());
+    }
+
+    private IEnumerator SpawnCoroutine(){
+        spawnInterval = Random.Range(minSpawnInterval, maxSpawnInterval);
+        yield return new WaitForSeconds(spawnInterval);
+        SpawnEnemy();
+        spawner = null;
+    }
+
+    private void SpawnEnemy(){
+         //Instantiate(bullet, transform.position, transform.rotation);
+        GameObject enemy = ObjectPoolManager.Instance.GetPooledObject("Enemy");
+        if(enemy != null){
+            enemy.transform.position = GetSpawnPosition();
+            enemy.gameObject.SetActive(true);
+        }
+    }
+
+    private Vector2 GetSpawnPosition(){
+        // Generate a random position along an axis
+        float xRandomPosition = Random.Range(-boundary.Bounds.x, boundary.Bounds.x);
+        float yRandomPosition = Random.Range(-boundary.Bounds.y, boundary.Bounds.y);
+        int areaToSpawn = Random.Range(0, 4);
+        switch(areaToSpawn){
+            // Upper part
+            case 0:
+                return new Vector2(xRandomPosition, boundary.Bounds.y);
+            //Right part
+            case 1:
+                return new Vector2(boundary.Bounds.x, yRandomPosition);
+            // Bottom part
+            case 2:
+                return new Vector2(xRandomPosition, -boundary.Bounds.y);
+            //Left part
+            case 3:
+                return new Vector2(-boundary.Bounds.x, yRandomPosition);
+        }
+        return Vector2.zero;
+    }
 }
