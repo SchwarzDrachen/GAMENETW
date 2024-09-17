3895702562f5a6c4b828d0bd4d8f6b42a20455fd
JmBears <115405999+JmBears@users.noreply.github.com>
2024-09-17 19:11:33 +0800

    [ADDED] HP bars for enemies
    still trying to make the collision work n stuff

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,9 +4,27 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 5.0f;
+    [SerializeField] private float moveSpeed = 2.0f;
+
+    float currentHealth, maxHealth;
 
     private Transform target = null;
+
+    [SerializeField] HealthManager healthBar;
+
+    private void Awake()
+    {
+        healthBar = GetComponentInChildren<HealthManager>();
+    }
+
+    private void Start()
+    {
+        //  I set maxHealth to random (DELETE ME)
+        maxHealth = Random.Range(1f, 4f);
+        currentHealth = maxHealth;
+        healthBar.UpdateHealthBar(currentHealth, maxHealth);
+        target = GameObject.Find("Player").transform;
+    }
 
     private void OnEnable(){
         LookAtTarget();
@@ -18,6 +36,12 @@ public class Enemy : MonoBehaviour
 
     private void Update(){
         Move();
+
+        //  If gameObject collides with prefab "playerBullet", take damage
+        if (Physics2D.Raycast(transform.position, Vector2.zero, 0.5f, LayerMask.GetMask("PlayerBullet")))
+        {
+            TakeDamage(10f);
+        }
     }
     
     private void LookAtTarget(){
@@ -31,5 +55,19 @@ public class Enemy : MonoBehaviour
 
     private void Move(){
         transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
+    }
+
+    //  Enemy takes damage from bullet function (DELETE ME)
+    public void TakeDamage(float damage)
+    {
+        currentHealth -= damage;
+        healthBar.UpdateHealthBar(currentHealth, maxHealth);
+        if (currentHealth <= 0)
+        {
+            //  Figure out how to kill the enemy without destroying gameobject?
+            //  Use the object pooling thing?
+            Debug.Log("ENEMY KILLED");
+            gameObject.SetActive(false);
+        }
     }
 }
